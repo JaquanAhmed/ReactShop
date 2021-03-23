d7ab8163b0c233914999ffb317e22e6e0e747113
JaquanAhmed <jaq-ahmed@hotmail.co.uk>
2021-03-23 22:46:52 +0000

    Updates

---
+++ precache-manifest.99ec6876200c55ff612449b50757dbc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930b3a54d2d73af23f985682f10878ee",
+    "url": "/ReactShop/index.html"
+  },
+  {
+    "revision": "f9b71776621384907a8a",
+    "url": "/ReactShop/static/css/main.e836be36.chunk.css"
+  },
+  {
+    "revision": "5fd8076190dc089a26fe",
+    "url": "/ReactShop/static/js/2.72f07810.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/ReactShop/static/js/2.72f07810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b71776621384907a8a",
+    "url": "/ReactShop/static/js/main.059b1cc1.chunk.js"
+  },
+  {
+    "revision": "6e660df064d3d03847a4",
+    "url": "/ReactShop/static/js/runtime-main.71ff193c.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ReactShop/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ReactShop/static/media/shopping-bag.30f07071.svg"
+  }
+]);
